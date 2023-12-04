2eda8b6fcbfde00f62e8575601ff11781f16f922
marcklen <marcklen@icloud.com>
2023-12-04 09:38:37 -0300

    renderizando componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -8,11 +8,18 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BigCardComponent implements OnInit {
 
   @Input()
-  photoCover: string ="https://th.bing.com/th/id/OIP.mRU-AbP-POroJu32ohVJOwAAAA?w=289&h=148&c=7&r=0&o=5&pid=1.7"
-  cardTitle: string ="NOVA VERSÃO DO ANGULAR JÁ DISPONÍVEL"
-  cardDescription: string ="Acesse o novo site do Angular para saber mais sobre as novidades e como atualizar seus projetos"
+  photoCover: string =""
+  @Input()
+  cardTitle: string =""
+  @Input()
+  cardDescription: string =""
+  
   constructor() {}
 
   ngOnInit(): void {
   }
 }
+
+/**
+ * 
+ */
